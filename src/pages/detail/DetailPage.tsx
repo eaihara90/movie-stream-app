6f93fb50914e68ffdfc956cf4710105feaf6e6e3
Eduardo Aihara <eaihara90@gmail.com>
2023-12-08 17:17:10 -0300

    feat: add movie detail page

---
+++ src/pages/detail/DetailPage.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import './DetailPage.scss';
+import { baseApi } from 'src/env';
+import { VideoDetail } from 'src/components/video-detail/VideoDetail';
+import { Stream } from 'src/components/stream/Stream';
+import { MovieModel } from 'src/models/movie.model';
+
+export function DetailPage(): JSX.Element {
+  const {id} = useParams<{ id: string }>();
+  const [activePage, setActivePage] = useState<'detail' | 'video'>('detail');
+  const [loading, setLoading] = useState<boolean>(false);
+  const [movie, setMovie] = useState<MovieModel>(new MovieModel());
+
+  useEffect(() => {
+    loadMovie();
+  }, [])
+
+  const loadMovie = async (): Promise<void> => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(`${baseApi}/movies/${id}`, { method: 'GET' });
+      const { movie } = await response.json();
+      setMovie(movie);
+      setLoading(false);
+    }
+    catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  }
+
+  return (
+    <div className="detail-page">
+      { (!loading && activePage === 'detail') && <VideoDetail movie={movie} onPlayVideo={() => setActivePage('video')}/>}
+      { (!loading && activePage === 'video') && <Stream movie={movie} onClose={() => setActivePage('detail')}/>}
+    </div>
+  );
+}
