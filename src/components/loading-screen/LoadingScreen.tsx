f3953a96e07c39ed84386a33027d7941bd08e0f6
Eduardo Aihara <eaihara90@gmail.com>
2023-12-10 20:02:44 -0300

    feat: add new component loading screen

---
+++ src/components/loading-screen/LoadingScreen.tsx
@@ -0,0 +1,9 @@
+import './LoadingScreen.scss';
+
+export function LoadingScreen(): JSX.Element {
+  return (
+    <div className="loading-screen">
+      <div className="loader"></div>
+    </div>
+  );
+}
