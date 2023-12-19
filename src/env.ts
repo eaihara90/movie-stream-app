f94a5f2f31ac27f175593636cf7313efeed454c9
Eduardo Aihara <eaihara90@gmail.com>
2023-12-19 17:33:19 -0300

    chore: change api hostname

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,7 @@
 const API_PROTOCOL = import.meta.env.VITE_API_PROTOCOL;
 const API_HOST = import.meta.env.VITE_API_HOST;
-const API_PORT = import.meta.env.VITE_API_PORT;
+// const API_PORT = import.meta.env.VITE_API_PORT;
 const API_PATH = import.meta.env.VITE_API_PATH;
 
-export const baseApi: string = `${API_PROTOCOL}://${API_HOST}:${API_PORT}/${API_PATH}`;
+export const baseApi: string = `${API_PROTOCOL}://${API_HOST}/${API_PATH}`;
+// export const baseApi: string = `${API_PROTOCOL}://${API_HOST}:${API_PORT}/${API_PATH}`;
